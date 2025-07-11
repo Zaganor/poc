173c4e9e18febe15f987a5525eafbba918cdc52c
Tiago <96320466+Zaganor@users.noreply.github.com>
2025-07-11 13:19:29 +0100

    Delete eth1.sh

--- eth1.sh
+++
@@ -1,7 +0,0 @@
-ip link add bond0 type bond
-ip link set bond0 type bond miimon 100 mode balance-rr
-ip link set eth1 down
-ip link set eth1 master bond0
-ip link set bond0 up
-ip link add link bond0 name bond0.10 type vlan id 10
-ip link set bond0.10 up
